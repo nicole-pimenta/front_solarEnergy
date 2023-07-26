ea2883c8fae232dc4b4b346e1ead7ce86ed85a12
Nicole <niipimenta@hotmail.com>
2023-07-26 00:00:15 -0300

    feat: estilização página calculation

--- src/pages/CalculationPage.tsx
+++ src/pages/CalculationPage.tsx
@@ -1,14 +1,91 @@
-import { Box, Typography } from "@mui/material";
-import { Footer } from "../components/Footer";
-import { Header } from "../components/Header";
+import { Box, Button, Container, Typography } from "@mui/material";
+import Slider from "@mui/material/Slider";
+import TextField from "@mui/material/TextField";
+import { useState } from "react";
 
 export const CalculationPage = () => {
   return (
-    <>
-      <Typography variant="subtitle1" mt={5}>
+    <Box
+      height={"100%"}
+      sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+    >
+      <Typography variant="h5" mt={3}>
         Make a simulation and find out which product is best for you
       </Typography>
-      calculation page
-    </>
+      <Typography variant="subtitle1" mb={5}>
+        Check here if you have the necessary area for installation of your solar
+        system
+      </Typography>
+
+      <Box
+        width={"80%"}
+        height={"75%"}
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          gap: "10px",
+          alignItems: "flex-start",
+        }}
+      >
+        <Box
+          height={"100%"}
+          width={"60%"}
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Typography variant="subtitle1">
+            Enter the requested data below
+          </Typography>
+
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              "& > :not(style)": { m: 1 },
+            }}
+          >
+            <TextField
+              helperText="Please enter the power in kw"
+              id="demo-helper-text-aligned"
+              label="Kw(kiloWatt)"
+            />
+            <TextField
+              helperText="Please enter the width of the space"
+              id="demo-helper-text-aligned"
+              label="Width (meters)"
+            />
+            <TextField
+              helperText="Please enter the height of the space "
+              id="demo-helper-text-aligned"
+              label="heigth  (meters)"
+            />
+          </Box>
+          <Button variant="contained" color="primary">
+            Simulate
+          </Button>
+        </Box>
+
+        <Box
+          bgcolor={"teal"}
+          height={"60%"}
+          width={"40%"}
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            gap: "10px",
+            alignItems: "center",
+          }}
+          style={{ borderRadius: "5px", boxShadow: "8px 5px 5px black" }}
+        >
+          <Typography variant="subtitle1" mb={5}>
+            ... Huum, you haven't done a simulation yet.
+          </Typography>
+        </Box>
+      </Box>
+    </Box>
   );
 };
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -8,71 +8,69 @@ import { useAppThemeContext } from "../contexts";
 export const HomePage = () => {
   const { themeName } = useAppThemeContext();
   return (
-    <>
-      <Box height={"100%"} sx={{ display: "flex", alignItems: "center" }}>
-        <Container maxWidth="lg">
-          <Box
-            sx={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              flexGrow: 1,
-              margin: "40px",
+    <Box height={"100%"} sx={{ display: "flex", alignItems: "center" }}>
+      <Container maxWidth="lg">
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            flexGrow: 1,
+            margin: "40px",
 
-              gap: "40px",
-            }}
-          >
-            <Box width={"50%"}>
-              <Typography
-                variant="h2"
-                color={themeName === "dark" ? "white" : "black"}
-              >
-                SOLAR ENERGY
-              </Typography>
-              <Typography
-                variant="subtitle1"
-                mt={5}
-                color={themeName === "dark" ? "white" : "black"}
-              >
-                RENEWABLE ENERGY AS A SOURCE.
-              </Typography>
-              <Typography
-                variant="subtitle1"
-                mt={5}
-                color={themeName === "dark" ? "white" : "black"}
-              >
-                AS A INVESTMENT.
-              </Typography>
-              <Typography
-                variant="subtitle1"
-                mt={5}
-                mb={5}
-                color={themeName === "dark" ? "white" : "black"}
-              >
-                AS EARNING.
-              </Typography>
+            gap: "40px",
+          }}
+        >
+          <Box width={"50%"}>
+            <Typography
+              variant="h2"
+              color={themeName === "dark" ? "white" : "black"}
+            >
+              SOLAR ENERGY
+            </Typography>
+            <Typography
+              variant="subtitle1"
+              mt={5}
+              color={themeName === "dark" ? "white" : "black"}
+            >
+              RENEWABLE ENERGY AS A SOURCE.
+            </Typography>
+            <Typography
+              variant="subtitle1"
+              mt={5}
+              color={themeName === "dark" ? "white" : "black"}
+            >
+              AS A INVESTMENT.
+            </Typography>
+            <Typography
+              variant="subtitle1"
+              mt={5}
+              mb={5}
+              color={themeName === "dark" ? "white" : "black"}
+            >
+              AS EARNING.
+            </Typography>
 
-              <Button variant="contained" color="primary">
-                <Link
-                  href="/calculation"
-                  underline="hover"
-                  color="inherit"
-                  padding={"5px"}
-                >
-                  Learn More
-                </Link>
-              </Button>
-            </Box>
-
-            <img
-              src={solarPanel}
-              height={"350px"}
-              alt="Solar panel image"
-              style={{ borderRadius: "5px", boxShadow: "10px -8px teal" }}
-            />
+            <Button variant="contained" color="primary">
+              <Link
+                href="/calculation"
+                underline="hover"
+                color="inherit"
+                padding={"5px"}
+              >
+                Learn More
+              </Link>
+            </Button>
           </Box>
-        </Container>
-      </Box>
-    </>
+
+          <img
+            src={solarPanel}
+            height={"350px"}
+            alt="Solar panel image"
+            style={{ borderRadius: "5px", boxShadow: "10px -8px teal" }}
+          />
+        </Box>
+      </Container>
+    </Box>
   );
 };
