98beca88afee217ee1ca9a189c06407bd2f7980e
Nicole <niipimenta@hotmail.com>
2023-07-26 12:54:00 -0300

    feat:responsividade homePage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,18 +1,25 @@
 import { Box, Button, Container } from "@mui/material";
 import solarPanel from "../assets/solarPanel.jpg";
 import { Typography } from "@mui/material";
-
 import { Link } from "@mui/material";
 import { useAppThemeContext } from "../contexts";
 
 export const HomePage = () => {
   const { themeName } = useAppThemeContext();
+
   return (
     <Box height={"100%"} sx={{ display: "flex", alignItems: "center" }}>
       <Container maxWidth="lg">
         <Box
           sx={{
             display: "flex",
+            flexDirection: {
+              xl: "row",
+              lg: "row",
+              sm: "column",
+              md: "column",
+              xs: "column",
+            },
             justifyContent: "center",
             alignItems: "center",
             flexGrow: 1,
@@ -21,7 +28,7 @@ export const HomePage = () => {
             gap: "40px",
           }}
         >
-          <Box width={"50%"}>
+          <Box max-width={"100%"}>
             <Typography
               variant="h2"
               color={themeName === "dark" ? "white" : "black"}
@@ -63,12 +70,15 @@ export const HomePage = () => {
             </Button>
           </Box>
 
-          <img
-            src={solarPanel}
-            height={"350px"}
-            alt="Solar panel image"
-            style={{ borderRadius: "5px", boxShadow: "10px -8px teal" }}
-          />
+          <Box max-width={"50%"}>
+            <img
+              src={solarPanel}
+              height={"350px"}
+              width={"100%"}
+              alt="Solar panel image"
+              style={{ borderRadius: "5px", boxShadow: "10px -8px teal" }}
+            />
+          </Box>
         </Box>
       </Container>
     </Box>
